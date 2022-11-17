a24d33426091eb79b63f01f11750d0a3b2f4e64c
TheHCL <daniel8411222@gmail.com>
2022-11-17 17:00:19 +0800

    expand cab file

--- hlk.py
+++ hlk.py
@@ -2,7 +2,7 @@ import wget,os
 import datetime
 from tkinter import messagebox
 
-hlk_filter="https://docs.microsoft.com/en-us/windows-hardware/test/hlk/user/windows-hardware-lab-kit-filters"
+hlk_filter="https://go.microsoft.com/fwlink/?linkid=875139"
 hlk_playlist="https://aka.ms/HLKPlaylist"
 
 now= datetime.datetime.now()
@@ -20,8 +20,11 @@ else:
 for x in files:
 		if os.path.isfile(x):
 			os.remove(x)
-
+t=os.getcwd()
 wget.download(hlk_filter,"filter.cab")
 wget.download(hlk_playlist,"playlist.zip")
+os.system("Expand filter.cab -F:* "+t)
+os.remove("filter.cab")
+os.remove("Readme.txt")
 
 messagebox.showinfo(title="Success",message="File download complete.")
